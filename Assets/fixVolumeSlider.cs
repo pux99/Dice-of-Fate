f75f0728dcf29fa4451e14634ae68d8aeb477e1c
IgnacioPuk <49323352+pux99@users.noreply.github.com>
2024-06-24 14:23:17 -0300

    slider fixer

--- Assets/fixVolumeSlider.cs
+++ Assets/fixVolumeSlider.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class fixVolumeSlider : MonoBehaviour
 {
-    
+    Slider slider;
+    public enum musicOrSound
+    {
+        Music,
+        sound
+    }
+    public musicOrSound sound;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+        if (sound == musicOrSound.sound)
+        {
+            SoundManager.soundVolumeChange.AddListener(fixSlider);
+        }
+        else
+        {
+            SoundManager.musicVolumeChange.AddListener(fixSlider);
+        }
+
+    }
+    public void fixSlider(float volumen)
+    {
+        slider.value = volumen;
+    }
 }
