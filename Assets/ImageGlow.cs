1650f50df9dd7d66157e93216a1ac09f98881a92
IgnacioPuk <49323352+pux99@users.noreply.github.com>
2024-06-30 13:04:03 -0300

    glow flips

--- Assets/ImageGlow.cs
+++ Assets/ImageGlow.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ImageGlow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Button buton;
+    public Image glow;
+    public float speed;
+    public float maxValue;
+    private bool ascendiong;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (buton.interactable)
+        {
+            if (glow.color.a >= maxValue)
+            {
+                ascendiong = false;
+            }
+            if (glow.color.a <= 0)
+            {
+                ascendiong=true;
+            }
+            if (ascendiong)
+                glow.color = new Color(glow.color.r, glow.color.g, glow.color.b, glow.color.a + speed * Time.deltaTime);
+            else
+                glow.color = new Color(glow.color.r, glow.color.g, glow.color.b, glow.color.a - speed * Time.deltaTime);
+        }
+        else
+            glow.color = new Color(glow.color.r, glow.color.g, glow.color.b, 0);
+
+
     }
 }
